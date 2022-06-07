58347b98fb5a82dba4f61211be1988571875fed7
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-06-07 03:09:04 -0500

    Update agModule.cs

--- source/agModule.cs
+++ source/agModule.cs
@@ -1,16 +1,10 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using UnityEngine;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
-using KSP.IO;
 using KSP;
+using KSP.UI.Screens;
 
 namespace RoverAntiGravitySystem
 {
